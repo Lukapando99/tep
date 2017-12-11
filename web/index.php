4e2832bc55458074ee4668ba3a957b4b0da83bb1
Lukapando99 <33448944+Lukapando99@users.noreply.github.com>
2017-12-11 11:30:10 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,102 +1,26 @@
+<!DOCTYPE html>
+
 <html>
-	<head>
-		<title>Ricerca</title>
-		<link rel="stylesheet" type="text/css" href="style.css">
-		<script>
-			function controllo_campi()
-			{
-				var valore=document.getElementById("lim").value;
-				var esito=false;
-				var verifica=/^\d{1,2}$/
-				if(document.getElementById("lim").value!=""&&document.getElementById("cit").value!=""&&document.getElementById("que").value!="")
-					if(valore.match(verifica)&&parseInt(valore)<51)
-						esito=true;
-				return esito;
-			}
-		</script>
-	</head>
-	<body>
-		<?php
-			if(isset($_POST["lim"]))
-			{
-				$lim=$_POST["lim"];
-			}
-			else
-			{
-				$lim=10;
-			}
-			if(isset($_POST["cit"]))
-			{
-				$cit=$_POST["cit"];
-			}
-			else
-			{
-				$cit="bergamo";
-			}
-			if(isset($_POST["que"]))
-			{
-				$que=$_POST["que"];
-			}
-			else
-			{
-				$que="pizzeria";
-			}
-		
-			echo "<form id='forma' method='post' onsubmit='return controllo_campi();'><br/>";
-			echo "<table>";
-			echo "<tr>";
-			echo " <td>Numero elementi (1-50): </td><td><input type='text' value='$lim' name='lim'id='lim' /></td>";
-			echo "</tr>";
-			echo "<tr>";
-			echo " <td>Citta: </td><td><input type='text' value='$cit' name='cit' id='cit' /></td>";
-			echo "</tr>";
-			echo "<tr>";
-			echo " <td>Cosa stai cercando?: </td><td><input type='text' value='$que' name='que' id='que' /></td><br/>";
-			echo "</tr>";
-			echo "</table>";
-			echo " <input type='submit' value='Aggiorna tabella' class='btn'/>";
-			echo "</form>";
-		
-			# Questo script chiama un'API e la inserisce in una tabella 
-			# Indirizzo dell'API da richiedere
-	                $indirizzo_pagina="https://api.foursquare.com/v2/venues/search?v=20161016&query=$que&limit=$lim&intent=checkin&client_id=YVMN1NGHAW4DWINOY2BHBVQTGR0RG01D4EVZ3Z3TPRN5EBE2&W&client_secret=GYRAVQCTVV5DUYI3J3OH2GKLQN5S2LEA0QIGECJ1MUFBTX2X&near=$cit";
-		        //$indirizzo_pagina="https://api.foursquare.com/v2/venues/search?v=20161016&query=$que&limit=$lim&intent=checkin&client_id=3AGUTWIPEQWCBRFCAHIN104WCY0IAPETGLTQIJUDP0JMIC5W&W&client_secret=NH1JI30DQ4YSF5PBSWMCTGPWLHBR1Z11VHYI5ELMV2MNAXNV&near=$cit";
-			# Codice di utilizzo di cURL
-			# Chiama l'API e la immagazzina in $json
-			$ch = curl_init() or die(curl_error());
-			curl_setopt($ch, CURLOPT_URL,$indirizzo_pagina);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			$json=curl_exec($ch) or die(curl_error());
-			# Decodifico la stringa json e la salvo nella variabile $data
-			$data = json_decode($json);
-			# Stampa della tabella delle pizzerie.
-			echo "<table>";
-				echo "<tr>";
-					echo "<th>NOME</th>";
-					echo "<th>LATITUDINE</th>";
-					echo "<th>LONGITUDINE</th>";
-				echo "</tr>";
-				for($i=0; $i<$lim; $i++)
-				{	
-					echo "<tr>";
-						echo "<td>";
-						echo $data->response->venues[$i]->name;
-						echo "</td>";
-						echo "<td>";
-						echo $data->response->venues[$i]->location->lat;
-						echo "</td>";
-						echo "<td>";
-						echo $data->response->venues[$i]->location->lng;
-						echo "</td>";
-					echo "</tr>";
-				}
-			echo "</table>";
-			# Stampa di eventuali errori
-			echo curl_error($ch);
-			curl_close($ch);
-			
-			
-		?>
-	</body>
+
+<body>
+<form method="post" action="esegui.php">
+
+  <!-- CASELLE DI TESTO -->
+  Nome<br>
+  <input type="text" name="nome"><br>
+ 
+  <!-- SELECTBOX -->
+  Paese<br>
+  <select name="paese">
+  <option value="B">Bergamo</option>
+  <option value="M">Milano</option>
+  <option value="L">Livigno</option>
+  </select><br><br>
+
+  <!-- SUBMIT -->
+  <input type="submit" name="invia" value="Invia i dati">
+
+</form>
+
+</body>
 </html>
-Credit to Gabriele Barcella
